a827adf33c06693be8f8a93ce03f5bdc242e302c
diegowdson <diegowdson@hotmail.com>
2023-08-16 16:04:18 -0300

    Finalizado o projeto do gerenciamento de tarefas

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,70 @@
+class TasksController < ApplicationController
+  include Exportable
+
+  before_action :set_task, only: %i[ edit update destroy ]
+
+  # GET /tasks or /tasks.json
+  def index
+    @tasks = Task.only_parents.order(:due_date)
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks or /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_path, notice: 'Tarefa foi criada com sucesso.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        flash.now[:alert] = @task.errors.full_messages.to_sentence
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1 or /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_path, notice: 'Tarefa atualizada com sucesso.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        flash.now[:alert] = @task.errors.full_messages.to_sentence
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1 or /tasks/1.json
+  def destroy
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Tarefa foi removida com sucesso.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:description, :due_date, :done, :parent_id)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  root 'tasks#index'
+
+  resources :tasks, except: %i[show] do 
+    collection do
+      get :export_csv
+    end
+  end 
+end
